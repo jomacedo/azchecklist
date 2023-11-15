9fc1e7f63b055c61a8f900bffd8a1189a92868d7
João Carlos Santos <jomacedo@microsoft.com>
2023-11-15 09:28:49 +0000

    UPD reference to checklists

--- src/service/GHDownloader.ts
+++ src/service/GHDownloader.ts
@@ -4,7 +4,7 @@ class GHDownloader {
     getAllFiles(): Promise<IGHFileList> {
         //TODO: The 'number' at the end is icky --> see if we can get this some other way
         //found this through: https://api.github.com/repos/Azure/review-checklists/git/trees/main?recursive=1
-        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/a068a9c66b559e668393306849bef9661d82f0fa")
+        return fetch("https://api.github.com/repos/Azure/review-checklists/git/trees/aa6279ffc9f440f968ebbe0d6a33cdd95f3b6725")
             .then(response => response.json());
     }
 }
